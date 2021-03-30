bff85ee087e73fb8cf96300ac2c8e4f17ef0fb55
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-03-30 23:39:07 +0900

    improve code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,11 +3,11 @@ class Bar<T1, T2, T3, T4, T5, T6>(
     private val getFoo: () -> Foo<T1, T2, T3, T4, T5, T6>,
 )
 
-fun test() {
-    val bar = Bar {
+fun <T1, T2, T3, T4, T5, T6> test(): Bar<T1, T2, T3, T4, T5, T6> {
+    return Bar {
         when (1) {
-            1 -> Foo(1)
-            else -> Foo(1)
+            1 -> Foo()
+            else -> Foo()
         }
     }
 }
