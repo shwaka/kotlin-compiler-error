0ae1980ef083d26ea1307836cf8cdda1d6a45077
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-03-30 23:42:24 +0900

    use 'if' instead of 'when'

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,9 +5,10 @@ class Bar<T1, T2, T3, T4, T5, T6>(
 
 fun <T1, T2, T3, T4, T5, T6> test(): Bar<T1, T2, T3, T4, T5, T6> {
     return Bar {
-        when (1) {
-            1 -> Foo()
-            else -> Foo()
-        }
+        // when (1) {
+        //     1 -> Foo()
+        //     else -> Foo()
+        // }
+        if (true) Foo() else Foo()
     }
 }
