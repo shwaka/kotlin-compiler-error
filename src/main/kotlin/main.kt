2eaece6410ea5bd0ce320ce0fcae706d144ec97a
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-03-30 23:59:09 +0900

    simplify

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,8 @@
 class Foo<T1, T2, T3, T4, T5, T6>
 class Bar<T1, T2, T3, T4, T5, T6>(
-    private val getFoo: () -> Foo<T1, T2, T3, T4, T5, T6>,
+    private val foo: Foo<T1, T2, T3, T4, T5, T6>,
 )
 
 fun <T1, T2, T3, T4, T5, T6> test(): Bar<T1, T2, T3, T4, T5, T6> {
-    return Bar {
-        // when (1) {
-        //     1 -> Foo()
-        //     else -> Foo()
-        // }
-        if (true) Foo() else Foo()
-    }
+    return Bar(if (true) Foo() else Foo())
 }
